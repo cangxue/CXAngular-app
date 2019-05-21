722ebfa289f37a1eb125ba0890fcdf484f6fc860
xiaoma <752787086@qq.com>
2019-05-21 10:53:12 +0800

    Add Hero Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CXAngular-app';
+  // title = 'CXAngular-app';
+  title = 'Tour of Heroes';
 }
